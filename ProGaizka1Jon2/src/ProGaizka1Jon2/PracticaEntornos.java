2c73188e587ce6733904d90e38b0abf71d2eb5ce
Admin <Admin@DESKTOP-JA67JF7>
2024-04-11 16:36:56 +0200

    Inicio de aplixacion

---
+++ ProGaizka1Jon2/src/ProGaizka1Jon2/PracticaEntornos.java
@@ -0,0 +1,10 @@
+package ProGaizka1Jon2;
+
+public class PracticaEntornos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+	}
+
+}
---
+++ ProGaizka1Jon2/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ProGaizka1Jon2 {
+}
